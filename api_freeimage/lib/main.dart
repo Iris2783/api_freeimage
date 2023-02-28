9316633f16480c24357a97194fa0b05934f1184b
Iris2783 <iris.corp.2783@gmail.com>
2023-02-28 14:15:52 +0900

    initial commit

---
+++ api_freeimage/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PixabayPage(),
+    );
+  }
+}
+
+class PixabayPage extends StatefulWidget {
+  const PixabayPage({super.key});
+
+  @override
+  State<PixabayPage> createState() => _PixabayPageState();
+}
+
+class _PixabayPageState extends State<PixabayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
